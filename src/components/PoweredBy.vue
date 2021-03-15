8bfb345e0305c3d1fac08745f0a8e722fb6a526a
Gerard-Szulc <gerszulc05@gmail.com>
2021-03-16 00:10:58 +0100

    Moves powered by and logo.

---
+++ src/components/PoweredBy.vue
@@ -0,0 +1,14 @@
+<template>
+  powered by
+  <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg"/>
+</template>
+
+<script>
+export default {
+  name: 'PoweredBy'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="home">
-    powered by
-    <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg"/>
     <PopularMovies></PopularMovies>
     <Favorites></Favorites>
   </div>
