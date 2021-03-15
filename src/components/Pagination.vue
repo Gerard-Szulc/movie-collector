b26da9ab3620c4d22be7f318ecaf79ac8c8b2cde
Gerard-Szulc <gerszulc05@gmail.com>
2021-03-15 23:53:43 +0100

    Fixes pagination ux by disabling not hiding buttons.

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,28 +1,28 @@
 <template>
   <nav aria-label="Pagination" v-if="totalPages !== 0 && currentPage !== 0">
     <ul class="pagination justify-content-center">
-      <li v-if="previous >= 1" class="page-item">
+      <li class="page-item" :class="{'disabled': previous < 1}">
         <a class="page-link" href="#" @click.prevent="() => changePage(1)"><i
             class="bi-arrow-left-circle-fill"></i></a></li>
-      <li v-if="previous >= 1" class="page-item">
+      <li class="page-item" :class="{'disabled': previous < 1}">
         <a class="page-link" href="#" @click.prevent="() => changePage(previous)"><i
             class="bi-arrow-left-circle"></i>
         </a>
       </li>
-      <li v-if="previous >= 1" class="page-item">
-        <a class="page-link" href="#" @click.prevent="() => changePage(previous)">{{ previous }}</a>
+      <li class="page-item" :class="{'disabled': previous < 1}">
+        <a class="page-link" href="#" @click.prevent="() => changePage(previous)">{{ previous > 0 ? previous : '-' }}</a>
       </li>
       <li class="page-item">
         <a class="page-link active" aria-current="page">{{ currentPage }}</a>
       </li>
-      <li v-if="next <= totalPages" class="page-item">
-        <a class="page-link" href="#" @click.prevent="() => changePage(next)"> {{ next }}</a>
+      <li class="page-item" :class="{'disabled': next > totalPages}">
+        <a class="page-link" href="#" @click.prevent="() => changePage(next)"> {{ next > totalPages ? '-' : next }}</a>
       </li>
-      <li v-if="next <= totalPages" class="page-item">
+      <li class="page-item" :class="{'disabled': next > totalPages}">
         <a class="page-link" href="#" @click.prevent="() => changePage(next)"><i
             class="bi-arrow-right-circle"></i></a>
       </li>
-      <li v-if="next <= totalPages" class="page-item">
+      <li class="page-item" :class="{'disabled': next > totalPages}">
         <a class="page-link" href="#" @click.prevent="() => changePage(totalPages)"><i
             class="bi-arrow-right-circle-fill"></i>({{ totalPages }})</a>
       </li>
